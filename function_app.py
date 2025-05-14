551547e774300cef393d08b5ebd8bf4091901698
KNNDL <dumitrufetcu7@gmail.com>
2025-05-14 16:31:16 +0300

    Azure Functions

--- function_app.py
+++ function_app.py
@@ -5,12 +5,12 @@ from detect_image import main as detect_image_main
 
 app = func.FunctionApp()
 
-@app.function_name(name="UploadImage")
+@app.function_name(name="upload_image")
 @app.route(route="upload", auth_level=func.AuthLevel.ANONYMOUS)
 def upload_image(req: func.HttpRequest) -> func.HttpResponse:
     return upload_image_main(req)
 
-@app.function_name(name="DetectImage")
+@app.function_name(name="detect_image")
 @app.blob_trigger(arg_name="blob", path="images/{name}", connection="AzureWebJobsStorage")
 def detect_image(blob: func.InputStream):
     detect_image_main(blob)
